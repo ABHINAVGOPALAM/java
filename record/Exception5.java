fba44dc07572ba460bec8c780ee72e0981fe5dcd
ABHINAVGOPALAM <115263478+ABHINAVGOPALAM@users.noreply.github.com>
2023-09-14 17:21:44 +0530

    Update Exception5.java

--- record/Exception5.java
+++ record/Exception5.java
@@ -1,5 +1,39 @@
-class Myexception extends Throwable
+class MyException extends Throwable
 {
-	Myexception()
+		int detail;
+		MyException(int a)
+		{
+			detail=a;
+		}
+
+	public String toString()
+		{
+			return "MyException ["+detail+"]";
+		}
+}
+
+class Exception5
+{
+	static void compute(int a) throws MyException
 	{
-		throw new Exception(k
+		System.out.println("Called compute "+ a);
+		if(a>10)
+		throw new MyException(a);
+		System.out.println("Normal Exit");
+	}
+	public static void main(String args[])
+	{
+		try
+		{
+			compute(1);
+			compute(20);
+		}
+		catch(MyException e)
+		{
+			System.out.println("Caught"+e);
+		}
+	}
+}
+
+
+		
